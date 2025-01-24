c64b82e4d432c40c3a53f01f80c2bf01130013f1
petertasca <tascapeter@gmail.com>
2025-01-24 16:04:00 -0500

    horizontal infinite scroll implemented -- hover effect next

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,48 @@
 
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import './App.css';
 
 function App() {
 
-  const [bestsellers, setBestsellers] = React.useState([])
+  const [bestsellers, setBestsellers] = useState([])
 
-
-
-  React.useEffect(() => {
+  useEffect(() => {
     fetch('http://localhost:8000/books/')
     .then(res => res.json())
     .then(data => setBestsellers(data))
   }, [])
 
+  
+  // const [matches] = useState(window.matchMedia('(prefers-reduced-motion: reduce)').matches)
+  const  [animationState, setAnimationState] = useState('running')
 
+  
 
   const bestsellerElements = bestsellers.map(bestseller=> {
     return(
-      <div className='bestseller-element'>
+      <li className='bestseller-element' key={bestseller.title_id} >
         <img src={bestseller.imageLinks['smallThumbnail']} alt="bestseller-img" />
-      </div>
+      </li>
     )
   })
+
+
+
+
+
   return (
    
     <div className="App">
       <h2>Check out the latest bestsellers</h2>
       <div className="book-container">
-        <div className="book-scroller">
-          <div className="bestseller-group snaps-inline">
+        <div className="book-scroller"
+        
+        
+        {...(animationState ? {'data-animated': true} : {})}>
+          <ul className="bestseller-list book-scroller__inner">
               {bestsellerElements}
-          </div>
+              {bestsellerElements}
+          </ul>
         </div>
       </div>
     </div>
