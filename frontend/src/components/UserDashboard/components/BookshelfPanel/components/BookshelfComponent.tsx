1f650332f630195c0afd07831e22f676125b8395
petertasca <tascapeter@gmail.com>
2025-03-07 16:53:56 -0500

    image clip 40px from bottom

--- frontend/src/components/UserDashboard/components/BookshelfPanel/components/BookshelfComponent.tsx
+++ frontend/src/components/UserDashboard/components/BookshelfPanel/components/BookshelfComponent.tsx
@@ -15,21 +15,22 @@ const BookshelfComponent = (props: BookshelfProps) => {
     console.log('bookshelf component data:', bookshelf.titles)
 
     const bookshelfTitles = bookshelf.titles.map((bookshelfTitle) => {
-       return <li key={bookshelfTitle.title_id}>
+       return <li className='book-card-listElement' key={bookshelfTitle.title_id}>
                 <article className="book-card" >
                     <div className="book-card-front">
-                        <img src={bookshelfTitle.imageLinks.thumbnail} alt="book-card-cover" className='book-card-img' />
-                        <h2 className="book-card-title">{bookshelfTitle.title}</h2>
+                        <div className="img-overlay"><img src={bookshelfTitle.imageLinks.thumbnail} alt="book-card-cover" className='book-card-img' /></div>
+                        <div className="book-card-buttons">
+                            <LikeIcon></LikeIcon>
+                            <DislikeIcon></DislikeIcon>
+                        </div>
+                        <h3 className="book-card-title">{bookshelfTitle.title}</h3>
                         {bookshelfTitle.authors.map(author => (
                             <li className='card-author-listElement' key={author.author_id}><span className='card-author-name'>{author.name}</span></li>
                         ))}
                         <p className="book-card-ratings">{bookshelfTitle.averageRating} out of {bookshelfTitle.ratingsCount} ratings</p>
                     </div>
                     <div className="book-card-back">
-                        <div className="book-card-buttons">
-                            <LikeIcon></LikeIcon>
-                            <DislikeIcon></DislikeIcon>
-                        </div>
+                        
                     </div>
                 </article>
                 
