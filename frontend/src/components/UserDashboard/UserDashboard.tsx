48c1eb8b65dda172497ffc94c679090ed323d958
petertasca <tascapeter@gmail.com>
2025-03-08 09:56:14 -0500

    active class set on mobile toggle and navbar

--- frontend/src/components/UserDashboard/UserDashboard.tsx
+++ frontend/src/components/UserDashboard/UserDashboard.tsx
@@ -5,6 +5,7 @@ import MessagePanel from './components/MessagePanel/MessagePanel';
 import AccountPanel from './components/AccountPanel/AccountPanel';
 import ProfileNavbar from './components/ProfileNavbar/ProfileNavbar'
 import BookclubsPanel from './components/BookclubsPanel/BookclubsPanel'
+import { UserIcon } from '../common/Icons'
 
 
 const panels = [
@@ -18,19 +19,21 @@ const UserDashboard = () => {
     
     const [activeTab, setActiveTab] = useState(0)
     const [activeBookshelf, setActiveBookshelf] = useState<number>(4)
+    const [activeNavbar, setActiveNavbar] = useState(false)
    
     const isBookshelfTab = activeTab >= 3
     const PanelComponent = () => { return isBookshelfTab ? <Bookshelfpanel activeBookshelf={activeBookshelf} /> : panels[activeTab] || null} 
+    const handleNavbar = () => setActiveNavbar(prev => !prev)
 
 
     return(
         <div className='dashboard-container'>
 
             <main className='dashboard-main'>
-            <button className='dashboardNavbar-mobile-toggle'>Profile</button>
+                <button className={`dashboardNavbar-mobile-toggle ${activeNavbar ? 'active' : ''}`} onClick={handleNavbar}><UserIcon /></button>
                     <PanelComponent />
             </main>
-            <aside className='dashboard-navbar'>
+            <aside className={`dashboard-navbar ${activeNavbar ? 'active' : ''}`}>
                 <ProfileNavbar 
                     activeTab={activeTab} 
                     setActiveTab={setActiveTab} 
