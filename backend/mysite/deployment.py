58813fcd6b26fe0e46f8fc9ef986a73871df811e
petertasca <tascapeter@gmail.com>
2025-03-18 13:34:50 -0400

    replaced cors origin whitelist with cors allowed origins

---
+++ backend/mysite/deployment.py
@@ -0,0 +1,21 @@
+import os
+from .settings import *
+from .settings import BASE_DIR
+
+
+ALLOWED_HOSTS = [os.environment['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environment['WEBSITE_HOSTNAME']]
+DEBUG = False
+
+# ADD WHITENOISE MIDDLEWARE
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
