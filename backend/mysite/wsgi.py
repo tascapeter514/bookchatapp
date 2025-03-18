cd21941a6c4a898dc9fb2fbdceb1b7243b303149
petertasca <tascapeter@gmail.com>
2025-03-18 13:54:21 -0400

    django setting module for deploymented implemented

--- backend/mysite/wsgi.py
+++ backend/mysite/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+settings_module = 'mysite.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'crud.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
