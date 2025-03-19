1a746bfafaf53e330339b7b6339fcaf6bb604b1b
petertasca <tascapeter@gmail.com>
2025-03-19 09:24:11 -0400

    changed environment to environ in deployment.py

--- backend/mysite/deployment.py
+++ backend/mysite/deployment.py
@@ -3,8 +3,8 @@ from .settings import *
 from .settings import BASE_DIR
 
 
-ALLOWED_HOSTS = [os.environment['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environment['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 SECRET_KEY = os.environ['MY_SECRET_KEY']
 
