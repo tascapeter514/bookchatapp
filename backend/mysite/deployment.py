b0854819ea09fc061aaa0f3b23fdad0660d433db
petertasca <tascapeter@gmail.com>
2025-03-19 10:13:07 -0400

    absolute path for base_dir

--- backend/mysite/deployment.py
+++ backend/mysite/deployment.py
@@ -49,4 +49,8 @@ DATABASES = {
 }
 
 
-STATIC_ROOT = str(BASE_DIR/'staticfiles')
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Ensure this line exists
