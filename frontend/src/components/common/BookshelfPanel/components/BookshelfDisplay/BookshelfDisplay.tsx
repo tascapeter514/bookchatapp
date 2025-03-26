4cc21f1da560063ac658ca659c6ef524270d1edc
petertasca <tascapeter@gmail.com>
2025-03-26 10:29:49 -0400

    bookshelf display implemented

--- frontend/src/components/common/BookshelfPanel/components/BookshelfDisplay/BookshelfDisplay.tsx
+++ frontend/src/components/common/BookshelfPanel/components/BookshelfDisplay/BookshelfDisplay.tsx
@@ -1,62 +1,35 @@
 import {Bookshelf} from '../../../../../types'
-import { bookshelfData } from '../../../Context/BookshelfContext/BookshelfContext'
+import BookCard from '../../../../BookCard/BookCard'
 import './BookshelfComponent.css'
-import { LikeIcon, DislikeIcon, CancelIcon } from '../../../Icons'
 
 
-interface BookshelfProps {
-    bookshelf: Bookshelf
+
+interface Props {
+    children: Bookshelf
 }
 
 
-const BookshelfComponent = ({ bookshelf }: BookshelfProps) => {
+const BookshelfDisplay = ({ children }: Props) => {
 
-    const { deleteBook } = bookshelfData()
-
-    
-
-
-
-    const books = .map((bookshelfTitle) => {
-
-        
-       return <li className='book-card-listElement' key={bookshelfTitle.title_id}>
-                <article className="book-card" >
-                   
-                    <div className="img-overlay">
-                        <CancelIcon onClick={() => deleteBook(bookshelfTitle.title_id, bookshelf.bookshelf_id)}></CancelIcon>
-                        <img src={bookshelfTitle.imageLinks.thumbnail} alt="book-card-cover" className='book-card-img' />
-                        <div className="book-card-buttons">
-                            <LikeIcon></LikeIcon>
-                            <DislikeIcon></DislikeIcon>
-                        </div>
-                    </div>
-                       
-                    <div className="book-card-back">
-                        <h3 className="book-card-title">{bookshelfTitle.title}</h3>
-                        {bookshelfTitle.authors.map(author => (
-                            <li className='card-author-listElement' key={author.author_id}><span className='card-author-name'>{author.name}</span></li>
-                        ))}
-                        <p className="book-card-ratings">{bookshelfTitle.averageRating} out of {bookshelfTitle.ratingsCount} ratings</p>
-                        
-                    </div>
-                </article>
-                
-             </li>
-
-    })
-
-
-
-
-
+    const { books } = children
 
     return(
         <section className="bookshelf-container">
-            <ul className='bookshelf-title-list'>{bookshelfTitles}</ul>
+            <ul className='bookshelf-title-list'>
+                {books.map((bookElement) => {
+                    return(
+                        <li 
+                            className='book-card-listElement' 
+                            key={bookElement.id}
+                        >
+                            <BookCard>{bookElement}</BookCard>   
+                        </li>
+                    )})
+                }  
+            </ul>
         </section>
         
     )
 }
 
-export default BookshelfComponent
+export default BookshelfDisplay
