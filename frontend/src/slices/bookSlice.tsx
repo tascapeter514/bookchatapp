bcb2868d11ac4265a9ad7cd0c54220f929aa83b2
petertasca <tascapeter@gmail.com>
2025-04-04 09:02:07 -0400

    bookSlice implemented

---
+++ frontend/src/slices/bookSlice.tsx
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Book } from "../types";
+
+
+export type BookState = {
+    book: Book
+}
+
+const initialState = {
+
+    book: {
+        id: 0,
+        name: '',
+        publisher: '',
+        description: '',
+        ISBN_Identifiers: [],
+        averageRating: NaN,
+        ratingsCount: NaN,
+        imageLinks: {smallThumbnail: '', thumbnail: ''},
+        pageCount: NaN,
+        genres: {id: NaN, name: ''},
+        authors: []
+    }
+
+}
+
+const bookSlice = createSlice({
+    name: 'book',
+    initialState,
+    reducers: {
+        fetchBook: (state, action) => {
+            state.book = action.payload
+        }
+
+    }
+})
+
+
+export const { fetchBook } = bookSlice.actions
+export default bookSlice.reducer
