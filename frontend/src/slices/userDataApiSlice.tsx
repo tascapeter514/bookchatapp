1db655fa80b72469b84e26dfc340f3bfafa56a22
petertasca <tascapeter@gmail.com>
2025-04-08 15:07:39 -0400

    add bookclub query added to userDataApiSlice

--- frontend/src/slices/userDataApiSlice.tsx
+++ frontend/src/slices/userDataApiSlice.tsx
@@ -1,17 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { Bookclub, Bookshelf, Invitation } from "../types";
 const WEBSOCKET_URL = 'ws://localhost:8000'
-
-
-
+const ADDBOOKCLUB_URL = 'api/user/bookclub/'
 
 export interface UserData {
-
     type: string,
     bookclubs: Bookclub[],
     bookshelves: Bookshelf[],
     invitations: Invitation[]
-
 }
 
 
@@ -24,53 +20,43 @@ export const userDataApi = createApi({
             async onCacheEntryAdded(
                 userId,
                 { updateCachedData, cacheDataLoaded, cacheEntryRemoved},
-            ) {
+            )
+            {
                 const ws = new WebSocket(`${WEBSOCKET_URL}/ws/userData/${userId}`)
                 try {
 
                     await cacheDataLoaded
-
                     const listener = (event: MessageEvent) => {
-
                     const data = JSON.parse(event.data)
-                       
                     if (data.type === 'get_user_data') {
-
                         updateCachedData((draft) => {
-
                             if (!draft) {
                                 console.warn('Draft is undefined')
                                 return;
                             }
-
                             console.log('draft:', draft)
                             draft.bookclubs = data.bookclubs,
                             draft.bookshelves = data.bookshelves,
                             draft.invitations = data.invitations
                         });
                     }
-
                     }
-                       
-
-                      
-
                     ws.addEventListener('message', listener)
-
                 } catch (error) {
-
                     console.error('Error during websocket setup:', error);
-
-
-
                 }
 
                 await cacheEntryRemoved;
-
                 ws.close();
             }
+        }),
+        addBookclub: build.mutation({
+            query: ({bookclubName, userId}: { bookclubName: string, userId: number}) => ({
+                url: `${ADDBOOKCLUB_URL}/${userId}`,
+                method: 'POST',
+                body: bookclubName
 
-
+            })
         })
     })
 })
