d85fdf0d860fa8a924d14d85ac3882cf7caa346b
petertasca <tascapeter@gmail.com>
2025-04-10 10:26:41 -0400

    bookshelf modal layout implemented

--- frontend/src/components/Modals/BookshelfButtonModal/BookshelfButtonModal.tsx
+++ frontend/src/components/Modals/BookshelfButtonModal/BookshelfButtonModal.tsx
@@ -1,9 +1,10 @@
 import { BookmarkIcon } from '../../Icons'
+import { Bookshelf } from '../../../types'
 import Button from '../../Buttons/Button/Button'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../../store/store'
 import SearchFilter from '../../Search/SearchFilter/SearchFilter'
-// import FilterResults from '../../Search/FilterResults/FilterResults'
+import FilterResults from '../../Search/FilterResults/FilterResults'
 import searchReducer from '../../../reducers/searchReducer'
 import { useGetUserDataQuery, usePostBookMutation } from '../../../slices/userDataApiSlice'
 import { useRef, useReducer, memo } from 'react'
@@ -22,7 +23,10 @@ const BookshelfButtonModal = () => {
     const [search, dispatchSearch] = useReducer(searchReducer, {id: 0, value: ''})
     const ref = useRef<HTMLDialogElement>(null)
     const openModal = () => ref.current?.showModal()
-    const closeModal = () => ref.current?.close()
+    const closeModal = () => {
+        console.log('close click')
+        ref.current?.close()
+    }
     const addBook = () => console.log('add button click')
 
     console.log('bookshelf modal data:', data)
@@ -38,21 +42,17 @@ const BookshelfButtonModal = () => {
                 <h3>Add this book to your bookshelf</h3>
                     <hr />
                     {user ? 
-                        <main className="bookshelf-results">
+                        <>
                             <SearchFilter search={search} dispatchSearch={dispatchSearch}/>
-                            {/* <FilterResults search={search} dispatchSearch={dispatchSearch}>{bookshelves}</FilterResults> */}
-                        </main>
-                            
-                        
+                            <FilterResults search={search} dispatchSearch={dispatchSearch}>{data?.bookshelves as Bookshelf[]}</FilterResults>
+                        </>
                         : <span>You must be logged in to use this feature</span>}
-                <div className="button-wrapper">
+                <div className="bookshelf-dialog-button-wrapper">
                     <Button onClick={closeModal}>Cancel</Button>
                     <Button onClick={addBook}>Add</Button>
                 </div>
             </dialog>
-            
         </>
-
     )
 
 }
--- frontend/src/components/Search/FilterResults/FilterResults.tsx
+++ frontend/src/components/Search/FilterResults/FilterResults.tsx
@@ -1,77 +1,40 @@
-import './FilterResults.css'
-import { Dispatch } from 'react'
-import { BookshelfState } from '../../../reducers/bookshelfReducer'
-import { BookclubState } from '../../../reducers/bookclubReducer'
+import { SearchState, SearchAction } from '../../../reducers/searchReducer'
 import { Bookshelf, Bookclub } from '../../../types'
-import { Data } from '../../../reducers/dataReducer'
- import { SearchState, SearchAction } from '../../../reducers/searchReducer'
+import { Dispatch } from 'react'
+import './FilterResults.css'
+
 
 
 interface FilterProps {
-    children: BookshelfState | BookclubState,
+    children: Bookshelf[]
     search: SearchState,
     dispatchSearch: Dispatch<SearchAction>
 }
-
-interface ResultProps {
-    results: BookshelfState | BookclubState
-}
-
-const Results = ({results}: ResultProps) => {
-
-    return(
-        <ul className="search-results-list">
-            {results.map((result: Bookshelf | Bookclub) => {
-                
-            })}
-
-        </ul>
-        
-    )
-
-}
-
-
 
 
 const FilterResults = ({children, search, dispatchSearch}: FilterProps  ) => {
 
 
     
-    const results = children.data.filter(child => child.name.toLowerCase().includes(search.value.toLowerCase()))
+    const results = children?.filter(child => child.name.toLowerCase().includes(search.value.toLowerCase()))
 
-    const resultElements = results.map(
-            ((resultElement: Bookshelf | Bookclub | Data) => {
-
-        if (!resultElement) {
-            return null
-        }
-
-        return (
-            <li key={resultElement.id} >
-
-                <div className='search-result-wrapper'>
-                    <label htmlFor={resultElement.name}>{resultElement.name}</label>
-                    <input 
-                        type="radio" 
-                        className='search-result-input' 
-                        name='searchResultsGroup'
-                        onChange={() => dispatchSearch({type: 'CHECK_RESULT', payload: resultElement.id})}
-                    />
-                    
-                </div>
-                
-                
-
-            </li>
-
-        )}))
-
-
-    
     return (
         <ul className="search-results-list">
-            {resultElements}
+            {results?.map((result: Bookshelf | Bookclub) => {
+                return(
+                    <li key={result.id} >
+                        <div className='search-result'>
+                            <label htmlFor={result.name}>{result.name}</label>
+                            <input 
+                                type="radio" 
+                                className='search-result-input' 
+                                name='searchResultsGroup'
+                                onChange={() => dispatchSearch({type: 'CHECK_RESULT', payload: result.id})}
+                            />
+                        </div>
+                    </li>
+                )
+            })}
         </ul>
 
     )
