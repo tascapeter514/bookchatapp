e082a8b9173c0ae9e35d56cb3b9bc79285821670
petertasca <tascapeter@gmail.com>
2025-04-11 15:56:58 -0400

    invite modal refactor started

--- frontend/src/components/Modals/InviteModal/InviteModal.tsx
+++ frontend/src/components/Modals/InviteModal/InviteModal.tsx
@@ -1,122 +1,57 @@
 import './InviteModal.css'
-import { ActiveUser } from '../../../../types'
-import { Ref, useState, useEffect } from 'react'
-import SearchFilter from '../../../Search/SearchFilter/SearchFilter'
-import { bookclubData } from '../../../../context/BookclubContext/BookclubContext'
-import SearchFilterResults from '../../../Search/SearchFilterResults/SearchFilterResults'
-import Button from '../../../Buttons/Button/Button'
-import { userContext} from '../../../../context/UserContext/UserContext'
-import { v4 as uuidv4 } from 'uuid';
-import axios from 'axios'
+import { useRef, useState, useReducer } from 'react'
+import Button from '../../Buttons/Button/Button'
+import SearchFilter from '../../Search/SearchFilter/SearchFilter'
+import searchReducer from '../../../reducers/searchReducer'
+import UserResults from '../../Search/UserResults/UserResults'
 
 
 
-interface InviteModalProps {
-
-    closeInviteModal: () => void,
-    inviteRef: Ref<HTMLDialogElement>
-
-}
 
 
-const InviteModal = ({ closeInviteModal, inviteRef }: InviteModalProps) => {
-
-    const { parameters } = bookclubData()
-    const { id } = parameters
-    const [searchValue, setSearchValue] = useState('')
-    const [userSearchResults, setUserSearchResults] = useState<ActiveUser[]>([])
-    const [newMemberId, setNewMemberId] = useState<number>(NaN)
-    const { activeUser } = userContext()
+const InviteModal = () => {
 
 
-    const handleUserSelection = (id: number) => {
-        setNewMemberId(id)
-    }
+    const [search, dispatchSearch] = useReducer(searchReducer, {id: 0, value: ''})
+    const ref = useRef<HTMLDialogElement>(null)
+    const openInviteModal = () => ref.current?.showModal()
+    const closeInviteModal = () => ref.current?.close()
+    
 
-    useEffect(() => {
 
-        try {
-            const socket = new WebSocket('ws://localhost:8000/ws/users')
-
-            socket.onmessage = (event) => {
-                const data = JSON.parse(event.data)
-                if (data.type === 'get_users') {
-                    console.log('all users:', data.users_data)
-                    setUserSearchResults(data.users_data)
-                }
-            }
-
-            socket.onerror = (error) => {
-                console.error('Users websocket error:', error)
-            }
-
-            socket.onopen = () => console.log('Users websocket connected');
-            socket.onclose = () => console.log('Users websocket disconnected');
-
-            return () => socket.close()
-            
-            
-        } catch (err) {
-            console.log('Failed to initialize users websocket');
-            
-        }
-
-    }, [])
-
-    const inviteUser = async (selectedUser: number | string): Promise<void> => { 
-        console.log('selected user:', selectedUser)
-        
-        const invitation = {
-            invitation_id: uuidv4(),
-            accepted: 0,
-            created_at: new Date().toLocaleString(),
-            bookclub_id: id,
-            invited_by_id: activeUser.id,
-            invited_user_id: selectedUser
-        }
-
-        axios.post('http://localhost:8000/api/sendInvite', invitation)
-            .then(response => {
-                console.log(response.data)
-            })
-            .catch(error => console.log('Your invitation was not sent', error))
-
-        closeInviteModal()
-
-    }
+  
 
 
 
     return (
-        <dialog className='invite-modal' ref={inviteRef}>
-            <h3>Invite a user to your bookclub</h3>
-            <hr />
-            <section className='invite-user-content'>
-                <SearchFilter
-                    searchValue={searchValue}
-                    setSearchValue={setSearchValue}
-                />
-                <article className='suggested-user-list'>
-                    <SearchFilterResults
-                        variant='user'
-                        searchValue={searchValue}
-                        handleSelection={handleUserSelection}
-                        selectedElement={newMemberId}
 
-                    >
-                        {userSearchResults}
+        <>
+            <Button
+                onClick={openInviteModal}
+            >
+                + Invite
+            </Button>
+            <dialog className='invite-modal' ref={ref}>
+                <h3>Invite a user to your bookclub</h3>
+                <hr />
+                <section className='invite-user-content'>
+                    <SearchFilter search={search} dispatchSearch={dispatchSearch}/>
+                    <article className='suggested-user-list'>
+                        <UserResults />
 
-                    </SearchFilterResults>
-                    
-                    
-                </article>
-            </section>
-            <div className="button-wrapper">
-                <Button onClick={closeInviteModal}>Cancel</Button>
-                <Button onClick={async () => newMemberId && await inviteUser(newMemberId)}>Invite</Button>
+                        
+                        
+                        
+                    </article>
+                </section>
+                <div className="button-wrapper">
+                    <Button onClick={closeInviteModal}>Cancel</Button>
+                    {/* <Button onClick={async () => newMemberId && await inviteUser(newMemberId)}>Invite</Button> */}
 
-            </div>
-        </dialog>
+                </div>
+            </dialog>
+        </>
+        
     )
 
 }
@@ -173,4 +108,57 @@ export default InviteModal
                             //         console.error('Error sending invitation:', error)
                             //     }
                                 
+                            // }
+
+
+                            // useEffect(() => {
+
+                            //     try {
+                            //         const socket = new WebSocket('ws://localhost:8000/ws/users')
+                        
+                            //         socket.onmessage = (event) => {
+                            //             const data = JSON.parse(event.data)
+                            //             if (data.type === 'get_users') {
+                            //                 console.log('all users:', data.users_data)
+                            //                 setUserSearchResults(data.users_data)
+                            //             }
+                            //         }
+                        
+                            //         socket.onerror = (error) => {
+                            //             console.error('Users websocket error:', error)
+                            //         }
+                        
+                            //         socket.onopen = () => console.log('Users websocket connected');
+                            //         socket.onclose = () => console.log('Users websocket disconnected');
+                        
+                            //         return () => socket.close()
+                                    
+                                    
+                            //     } catch (err) {
+                            //         console.log('Failed to initialize users websocket');
+                                    
+                            //     }
+                        
+                            // }, [])
+                        
+                            // const inviteUser = async (selectedUser: number | string): Promise<void> => { 
+                            //     console.log('selected user:', selectedUser)
+                                
+                            //     const invitation = {
+                            //         invitation_id: uuidv4(),
+                            //         accepted: 0,
+                            //         created_at: new Date().toLocaleString(),
+                            //         bookclub_id: id,
+                            //         invited_by_id: activeUser.id,
+                            //         invited_user_id: selectedUser
+                            //     }
+                        
+                            //     axios.post('http://localhost:8000/api/sendInvite', invitation)
+                            //         .then(response => {
+                            //             console.log(response.data)
+                            //         })
+                            //         .catch(error => console.log('Your invitation was not sent', error))
+                        
+                            //     closeInviteModal()
+                        
                             // }
