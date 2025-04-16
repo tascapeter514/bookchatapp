0767442aaa44cf39ebd8f874ed717256f594b3a8
petertasca <tascapeter@gmail.com>
2025-04-15 20:20:05 -0400

    fix: carousel sliding off to left

--- frontend/src/components/Carousel/Carousel.tsx
+++ frontend/src/components/Carousel/Carousel.tsx
@@ -1,5 +1,7 @@
 import { Book, Author } from '../../types'
+import { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { RightArrow, LeftArrow } from '../Icons'
 import './Carousel.css'
 
 
@@ -11,18 +13,49 @@ interface Props {
 
 const Carousel = ({children}: Props) => {
 
+    const carouselRef = useRef<HTMLUListElement>(null);
+    const firstBookRef = useRef<HTMLLIElement>(null)
+    const [offset, setOffset] = useState(0)
 
-    const carouselElements = children.map((book: Book)=> {
+    const scroll = (direction: 'left' | 'right') => {
+        console.log('scroll check')
+        const container = carouselRef.current;
+        if (!container) return
+
+        const book = container.querySelector('.carousel-element') as HTMLLIElement | null
+        if (!book) return
+
+        const bookStyle = getComputedStyle(book)
+        const bookWidth = book.offsetWidth + parseFloat(bookStyle.marginRight || '0')
+
+        const scrollAmount = bookWidth * 5;
+        const maxScroll = container.scrollWidth - container.offsetWidth
+
+        setOffset(prevOffset => {
+            const newOffset =
+                direction === 'left'
+                ? Math.min(prevOffset + scrollAmount, 0)
+                : Math.max(prevOffset - scrollAmount, -maxScroll);
+            return newOffset;
+        })
+    }
+
+    //    setOffset(prevOffset => {
+    //     const newOffset = direction === 'left' ? prevOffset - scrollAmount : prevOffset + scrollAmount
+    //     return Math.max(Math.min(newOffset, 0), -(container.scrollWidth - container.offsetWidth))
+    //    })
+
+
+
+    const carouselElements = children.map((book: Book, index: number)=> {
         return(
 
-        <li className='carousel-element' key={book.id} >
+        <li className='carousel-element' key={book.id} ref={index === 0 ? firstBookRef : null}>
             <Link to={`/book/${book.id}`}>
                 <img src={book.imageLinks['smallThumbnail']} alt="carousel-img" className='carousel-image'/>
                 <small className='carousel-book-title'>{book.name}</small>
                 {book.authors?.map((author: Author) => (
-                    <li className='carousel-author-text'>
-                        <small>{author.name}</small>
-                    </li>
+                    <small className='carousel-author-text' key={author.id}>{author.name}</small>
                 ))}
             </Link>
         </li>
@@ -31,9 +64,13 @@ const Carousel = ({children}: Props) => {
 
     return(
 
-        <ul className='carousel'>
-            {carouselElements}
-        </ul>
+        <div className="carousel-container">
+            <LeftArrow onClick={() => scroll('left')} />
+            <RightArrow onClick={() => scroll('right')}/>
+            <ul className='carousel' ref={carouselRef} style={{ transform: `translateX(${offset}px)` }}>
+                {carouselElements}
+            </ul>
+        </div>
 
     )
 
