87a120b66e6d46293e4358eec9c51e22a572120e
petertasca <tascapeter@gmail.com>
2025-04-19 10:23:05 -0400

    feat: created poll app; refactor: deleted accounts api

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -17,8 +17,7 @@ urlpatterns = [
     path('api/auth/login', log_in, name='log_in'),
     path('api/auth/register', register, name='register'),
     path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
-    path('api/inviteusers/<uuid:bookclub_id>', InviteUsersAPI.as_view()),
     path('api/user/updateAccount/<int:id>', change_contact, name='change_contact'),
     path('api/user/updatePassword/<int:id>', change_password, name='change_password'),
-     path('api/bookclub/users/<int:id>', get_users, name='get_users'),
+    path('api/bookclub/users/<int:id>', get_users, name='get_users'),
 ]
--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookchat',
+    'polls',
     'rest_framework',
     'knox',
     'accounts',
