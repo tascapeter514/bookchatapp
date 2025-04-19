e4e0c75a7d08b035343f06ac6249129bf9956f7a
petertasca <tascapeter@gmail.com>
2025-04-19 10:32:56 -0400

    feat:poll routes and views

---
+++ backend/polls/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('api/polls/create', create, name='create_poll'),
+    path('api/poll/vote/<int: poll_id>/', vote, name='poll'),
+    path('api/polls/results/<int: poll_id>/', get_results, name='poll_results')
+]
--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+
+@api_view(['POST'])
+def create(request):
+    print('create poll')
+
+
+@api_view(['PUT'])
+def vote(request, id):
+    print('Vote on poll')
+
+@api_view(['GET'])
+def get_results(request, id):
+    print('get poll results')
