97c7e1f6c644e8ca834d754f480d1f360661d1ed
petertasca <tascapeter@gmail.com>
2025-04-29 10:02:57 -0400

    sentry re-added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,7 @@ import './index.css'
 import App from './App.tsx'
 
 
+
 Sentry.init({
   dsn: "https://db0d928e2ac562e04c7dd9a414449088@o4509236874641408.ingest.us.sentry.io/4509236879818752",
   integrations: [
@@ -15,7 +16,6 @@ Sentry.init({
   tracesSampleRate: 1.0, // 1.0 = 100% of transactions (can reduce in production)
   sendDefaultPii: true,
 });
-
 
 
 createRoot(document.getElementById('root')!).render(
