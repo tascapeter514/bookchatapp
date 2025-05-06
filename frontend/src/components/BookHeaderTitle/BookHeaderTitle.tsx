b9c0b3a0b6b6848964b1dfd50b3572f65572a4d7
petertasca <tascapeter@gmail.com>
2025-05-06 10:58:04 -0400

    debug: i is null

--- frontend/src/components/BookHeaderTitle/BookHeaderTitle.tsx
+++ frontend/src/components/BookHeaderTitle/BookHeaderTitle.tsx
@@ -1,16 +1,16 @@
 import { authorText } from '../../utils/textFormatting'
 import { Link } from 'react-router-dom'
 import { Book } from '../../types'
-import WithAddBookToBookclubLogic from '../HigherOrderComponents/WithAddBookToBookclubLogic'
-import WithAsync from '../HigherOrderComponents/WithAsync'
-import AddBookToBookclubModal from '../Modals/AddBookToBookclubModal/AddBookToBookclubModal'
+// import WithAddBookToBookclubLogic from '../HigherOrderComponents/WithAddBookToBookclubLogic'
+// import WithAsync from '../HigherOrderComponents/WithAsync'
+// import AddBookToBookclubModal from '../Modals/AddBookToBookclubModal/AddBookToBookclubModal'
 import './BookHeaderTitle.css'
 
 interface Props {
     book: Book
 }
 
-const AddBookToBookclubModalWithLogic = WithAddBookToBookclubLogic(WithAsync(AddBookToBookclubModal))
+// const AddBookToBookclubModalWithLogic = WithAddBookToBookclubLogic(WithAsync(AddBookToBookclubModal))
 
 
 
@@ -23,7 +23,7 @@ const BookHeaderTitle = ({book}: Props) => {
             <h1>{book.name}</h1>
             <h3>By <span> {authorText(book.authors)} </span></h3>
             <p>Category: <Link to='#' >{book.genres.name}</Link></p>
-            <AddBookToBookclubModalWithLogic bookId={book.id} />
+            {/* <AddBookToBookclubModalWithLogic bookId={book.id} /> */}
             
 
         </article>
