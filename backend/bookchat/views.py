453747cd1f67bc3cbaaecd935b67e739da1de18d
petertasca <tascapeter@gmail.com>
2025-05-13 12:53:59 -0400

    fix: author prefetch_related

--- backend/bookchat/views.py
+++ backend/bookchat/views.py
@@ -53,7 +53,7 @@ def get_books(request):
         print(f'fetching books from library at {current_time}')
 
         books = Book.objects.select_related('genres')\
-                            .prefetch_related('authors')\
+                            .prefetch_related('author')\
                             .exclude(imageLinks__smallThumbnail__isnull=True)\
                             .exclude(imageLinks__smallThumbnail__exact='')\
                             .filter(genres_id__in=desired_genres)
