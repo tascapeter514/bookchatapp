072fbbc32746caec280a06fdd31c195360c1ef02
petertasca <tascapeter@gmail.com>
2025-05-15 18:05:30 -0400

    fix: fetch priority high on author image

--- frontend/src/components/AuthorHeader/AuthorHeader.tsx
+++ frontend/src/components/AuthorHeader/AuthorHeader.tsx
@@ -12,7 +12,7 @@ const AuthorHeader = ({author}: Props) => {
 
     return(
         <div className="author-header">
-            <img src={author.authorPhoto} alt="" className="author-cover" />
+            <img src={author.authorPhoto} alt="" className="author-cover" fetchPriority='high' />
             <article className="author-info">
                 <h1>{author.name}</h1>
                 <p>Born on <span>{author.birthDate}</span></p>
